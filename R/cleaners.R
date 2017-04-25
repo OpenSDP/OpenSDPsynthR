b03d113be133e19295f74a0654d63d6e106f4026
Jared Knowles <jknowles@gmail.com>
2017-04-25 19:03:13 -0400

    prep for CEDS exporter

--- R/cleaners.R
+++ R/cleaners.R
@@ -100,10 +100,7 @@ sdp_cleaner <- function(simouts){
 
 ## Postsecondary wide
 
-
 # TODO - check why ps and ps_yr1-ps_yr4 are not connected
-
-
 
 # Figure out status indicators
 
@@ -152,3 +149,131 @@ sdp_cleaner <- function(simouts){
 # cg_student$highly_qualified[!is.na(cg_student$chrt_grad) &
 #                               is.na(cg_student$highly_qualified)] <- 0
 # table(is.na(cg_student$highly_qualified[is.na(cg_student$chrt_grad)]))
+#########################
+## CEDS
+######################
+
+
+# K12 Student Identity
+## - Student Identification System / demog_master$id_type = "Local"
+## - Student Identifier / demog_master$sid
+
+# K12 Student Demographic
+## - American Indian or ALaska Native / demog_master$American.Indian.or.Alaska.Native
+## - Asian / demog_master$Asian
+## - Birthdate / demog_master$Birthdate
+## - Black or African American / demog_master$ Black.or.African.American
+## - Demographic Race Two or More Races / demog_master$Demographic.Race.Two.or.More.Races
+## - Hispanic or Latino Ethnicity / demog_master$Hispanic.or.Latino.Ethnicity
+## - Native Hawaiian or Other Pacific Islander /demog_master$Native.Hawaiian.or.Other.Pacific.Islander
+## - Sex / demog_master$Sex
+## - White / demog_master$White
+
+# K12 Limited English Proficiency
+## - Limited English Proficiency Entry Date / stu_year$year
+## - Limited English Proficiency Exit Date / stu_year$year
+## - Limited English Proficiency Status / stu_year$ell
+## - Title III Limited English Proficient Participation Status / stu_year$ell
+
+# - K12 Student Economically DIsadvantaged
+## - Economic Disadvantage Status / stu_year$frpl
+## - Participation in School Food Service Programs / stu_year$frpl
+## - Status End Date / stu_year$year
+## - Status Start Date / stu_year$year
+
+
+# - K12 Student Enrollment
+## - Cohort Year / stu_year$cohort_year
+## - Cohort Graduation Year / stu_year$cohort_grad_year
+## - Enrollment Entry Date / stu_year$year
+## - Enrollment Exit Date / stu_year$year
+## - Enrollment Status / stu_year$ENROLLMENTSTATUS
+## - Entry Grade Level / stu_year$grade
+## - Entry Type / stu_year$entry_code
+## - Exit Grade Level / stu_year$grade
+## - Exit or Withdrawal Status / stu_year$exit_type
+## - Gifted and Talented Indicator / stu_year$gifted
+## - School Identifier / stu_year$schid
+## - Local Education Agency Identifier / stu_year$LEAID
+## - Responsible District Identifier / stu_year$LEAID
+## - Responsible School Identifier / stu_year$schid
+## - Responsible School Type / stu_year$LEAID
+## - Responsible District Type / stu_year$LEA_TYPE
+
+# - K12 Student Academic Record (long by term)
+## - Student Identifier / hs_annual$sid
+## - School Identifier / hs_annual$schid
+## - Credits earned cumulative / hs_annual$cum_credits
+## - Credits attempted cumulative / hs_annual$cum_creditsATTEMPTED?
+## - Diploma or Credential Award Date / hs_outcomes$GRAD_YEAR
+## - End of Term Status /  stu_year$PROMOTIONRETENTION
+## - GPA Cumulative / hs_annual$cum_gpa
+## - Graduation Rate Survey Cohort Year / hs_annual$year
+## - Graduation Rate Survey Indicator / hs_annual$grad_cohort_ind
+## - High school Diploma Type / hs_outcomes$diploma_type
+## - High school student class rank / hs_outcomes$class_rank
+## - Number of credits attempted / hs_annual$credits_attempted
+## - Number of credits earned / hs_annual$credits_earned
+## - Postsecondary Enrollment Action hs_outcomes$ps [Enrolled/NotENrolled/No Information]
+## - Projected Graduation Date / hs_annual$expected_grad
+
+# K12 Student Attendance
+## - Number of Days Absent - stu_year$ndays_possible
+## - NUmber of Days in Attendance / stu_year$ndays_attend
+## - Student Attendance Rate / stu_year$att_rate
+
+
+# - K12 School Identification
+## - Name of Institution / school$name
+## - Organization Type / school$type ??
+## - School Identification System / school$id_type = "District"
+## - School Identifier / school$schid
+## - Short name of institution / school$name
+
+# K12 School Institution Characteristics
+## - State Poverty Designation / school$poverty_desig = HighQuartile/LowQuartile/Neither
+## - Title I School Status / school$title1_status
+## - Title III Language Instruction Porgram Type / school$title3_program_type
+
+# PS Institution Directory
+## - Name of Institution / nsc$name
+## - Short Name of Institution / nsc$short_name
+
+# PS Student Institution Enrollment
+## - Enrollment Entry Date /  ps_enroll$year
+## - Enrollment Exit Date / ps_enroll$year
+## - Name of Institution / ps_enroll$nsc_inst_name
+## - Diploma or Credential Award Date / ps_enroll$ps_grad_date
+
+## Assessment
+## Assessment - Assessment Registration - School Identifier / assessment$schid
+## Assessment - Assessment Registration - Student Identifier  / assessment$sid
+## Assessment - Assessment Registration - Assessment Registration Grade Level to Be Assessed / assessment$grade
+## Assessment - Assessment Registration - Grade Level When Assessed / assessment$grade_enrolled
+## Assessment - Assessment Registration - Assessment Accomodation Category / assessment$accomodation
+## Assessment - Assessment Registration - Local Education Agency Identifer / assessment$LEA
+## Assessment - Assessment Registration - Retest Indicator / assessment$retest_ind
+
+# assessmentresult
+## - Assessment Result - Assessment Score Metric Type / assessment$score_type
+## - AssessmentResultScoreValue / assessment$score
+## - Assessment Result Score STandard Error / assessment$error
+
+# Assessment
+## Assessment Identifier / assessment$assess_id
+## Assessment Short Name / assessment$assess_name
+## Assessment Academic Subject / assessment$subject
+## Assessment Level for Which Designed / assessment$assessment_grade
+
+# assessment performance level
+## - Assessment Performance Level Identifier / assessment$prof_level
+## - Assessment Performance Level Label / assessment$prof_level_name
+## - Assessment Performance Level Lower Cut Score / assessment$min_score
+## - Assessment Performance Level Upper Cut Score / assessment$max_score
+
+
+# SEA Identification
+## Organization Name
+## Organization Type
+## State Agency Identifier
+

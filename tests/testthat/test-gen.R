f92fbb05fd16da334916bccff76d6f6bbcbe8c1b
Jared Knowles <jknowles@gmail.com>
2018-08-22 11:24:14 -0400

    updating unit tests

--- tests/testthat/test-gen.R
+++ tests/testthat/test-gen.R
@@ -4,34 +4,38 @@ context("Test high level data generation functions")
 
 #TODO make gen_students produce consistent number of columns
 test_that("gen_students produces students", {
-  out <- gen_students(n = 10, seed = 21421)
+  set.seed(21421)
+  out <- gen_students(n = 10)
   expect_equal(nrow(out), 10)
   expect_is(out, "data.frame")
   expect_identical(names(out),
                    c("sid", "Sex", "Birthdate", "Race", "White",
                      "Black.or.African.American",
                      "Hispanic.or.Latino.Ethnicity",
-                     "Demographic.Race.Two.or.More.Races"))
+                     "Demographic.Race.Two.or.More.Races", "id_type"))
   expect_is(out[, "sid"], "factor")
   expect_is(out[, "Sex"], "factor")
   expect_is(out[, "Birthdate"], "Date")
   expect_is(out[, "Race"], "factor")
   expect_is(out[, "White"], "character")
   expect_is(out[, "Black.or.African.American"], "character")
-  out <- gen_students(n = 100, seed = 21421)
+  out <- gen_students(n = 100)
   expect_equal(nrow(out), 100)
-  expect_equal(ncol(out), 10)
+  expect_equal(ncol(out), 11)
 })
 
 test_that("gen_student_years longitudinal data", {
-  out <- gen_students(n = 100, seed = 2352)
+  set.seed(2352)
+  out <- gen_students(n = 100)
   out_l <- gen_student_years(data = out)
   expect_message(gen_student_years(data = out), regexp = NA)
-  expect_equal(ncol(out_l), 4)
+  expect_equal(ncol(out_l), 8)
   # Figure out how to test number of rows produced
   expect_equal(min(out_l$year), sim_control()$minyear)
   expect_equal(max(out_l$year), sim_control()$maxyear)
-  expect_identical(names(out_l), c("sid", "year", "Birthdate", "age"))
+  expect_identical(names(out_l)[1:4], c("sid", "year", "Birthdate", "age"))
+  expect_identical(names(out_l)[5:8], c("enrollment_status", "cohort_grad_year",
+                                        "cohort_year", "exit_type"))
 })
 
 
